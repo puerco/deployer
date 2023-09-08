776b0c4245eb33c14066f2f4d0dc7d9b947fbb8c
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2023-09-08 17:59:49 -0600

    Data handling backend
    This commit adds the data backend to the document. Data is buffered in
    memory unless documents are large. When taht happesn data is handled using
    a temporary file.
    
    The document type now implements the ReadSeeker interface and can be used
    in all reader situations regardless if its working in memory or on disk.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/payload/document.go
+++ pkg/payload/document.go
@@ -1,5 +1,140 @@
 package payload
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+// maxInMemSize is the maximum document size that will be handled in
+// memory. If the document data grows after this threshold, all I/O
+// operations will be performed on a temporary file.
+const maxInMemSize = 2097152
+
 // Document is an object that abstracts a security document handled by
 // deployer.
-type Document struct{}
+type Document struct {
+	Format  Format
+	tmpFile *os.File
+	data    []byte
+	reader  *bytes.Reader
+}
+
+func NewDocument() *Document {
+	return &Document{}
+}
+
+func NewDocumentFromFile(path string) (*Document, error) {
+	doc := NewDocument()
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("opening file to create new document: %w", err)
+	}
+	if err := doc.ReadData(f); err != nil {
+		return nil, fmt.Errorf("reading new document data: %w", err)
+	}
+	return doc, nil
+}
+
+// ReadData takes an io.Reader r and ingests the data of the document
+// from it using Read(). Data will be kept in memory until maxInMemSize
+// bytes are read, after which data will be dumped to a temporary file.
+func (d *Document) ReadData(r io.Reader) (err error) {
+	d.data = []byte{}
+	d.tmpFile = nil
+
+	for {
+		b := make([]byte, 32768)
+		rs, readErr := r.Read(b)
+		if readErr != nil {
+			if readErr != io.EOF {
+				return fmt.Errorf("reading document data: %w", readErr)
+			}
+			break
+		}
+
+		switch d.tmpFile == nil {
+		case false:
+			if _, err := d.tmpFile.Write(b[0:rs]); err != nil {
+				return fmt.Errorf("writing data to document file: %w", err)
+			}
+		case true:
+			d.data = append(d.data, b[0:rs]...)
+
+			if len(d.data) > maxInMemSize {
+				f, err := os.CreateTemp("", "deployer-payload-*.raw")
+				if err != nil {
+					return fmt.Errorf("creating temporary file to store payload document: %w", err)
+				}
+				s, err := f.Write(d.data)
+				if err != nil {
+					return fmt.Errorf("dumping memory buffer to disk: %w", err)
+				}
+				if s != len(d.data) {
+					return errors.New("dumping document data resulted in short read")
+				}
+				d.tmpFile = f
+				d.data = nil
+			}
+		}
+	}
+
+	if d.tmpFile == nil {
+		d.reader = bytes.NewReader(d.data)
+	}
+	if _, err := d.Seek(0, 0); err != nil {
+		return fmt.Errorf("seeking newly buffered data: %w", err)
+	}
+	return err
+}
+
+// Read implements the reader interface to be able to use the document
+// wherever io.Reader fits
+func (d *Document) Read(b []byte) (n int, err error) {
+	if d.tmpFile != nil {
+		return d.tmpFile.Read(b)
+	}
+
+	return d.reader.Read(b)
+}
+
+func (d *Document) Seek(offset int64, whence int) (int64, error) {
+	if d.tmpFile != nil {
+		return d.tmpFile.Seek(offset, whence)
+	}
+
+	return d.reader.Seek(offset, whence)
+}
+
+func (d *Document) Hash() (hVal string, err error) {
+	if _, err := d.Seek(0, 0); err != nil {
+		return "", fmt.Errorf("rewinding document: %w", err)
+	}
+	h := sha256.New()
+	defer func() { _, err = d.Seek(0, 0) }()
+	if _, err := io.Copy(h, d); err != nil {
+		return hVal, fmt.Errorf("copying data to hash")
+	}
+	return fmt.Sprintf("%x", h.Sum(nil)), err
+}
+
+// Cleanup is a function intended to be run just before a document is discarded.
+// It takes care of closing its file and
+func (d *Document) Cleanup() {
+	d.data = nil
+	d.reader = nil
+	if d.tmpFile == nil {
+		return
+	}
+	d.tmpFile.Close() //nolint
+	os.Remove(d.tmpFile.Name())
+}
+
+// inMemory is an internal method to query the status of the data
+func (d *Document) inMemory() bool {
+	return d.tmpFile == nil
+}
